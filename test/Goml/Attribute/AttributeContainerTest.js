b0b058cf6769c95dd3e9215dda58bb2d7c048cc6
pnlybubbles <pnlybubbles@gmail.com>
2016-05-02 17:17:36 +0900

    [from now] 2016/05/02 17:17:36
    diff --git a/test/Goml/Attribute/AttributeContainerTest.js b/test/Goml/Attribute/AttributeContainerTest.js
    index 6dd85e3..7ff6195 100644
    --- a/test/Goml/Attribute/AttributeContainerTest.js
    +++ b/test/Goml/Attribute/AttributeContainerTest.js
    @@ -32,3 +32,48 @@ test('Construct attribute container with correct attribute count (many)', (t) =>
       const attributesContainer = new AttributesContainer(element);
       t.truthy(Object.keys(attributesContainer._members).length === 3);
     });
    +
    +test('If value of attribute "id" is set, value will be correctly changed.', (t) => {
    +  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
    +  const attributesContainer = new AttributesContainer(element);
    +  t.truthy(attributesContainer.get('id') === 'id_test');
    +  attributesContainer.set('id', 'id_test_changed');
    +  t.truthy(attributesContainer.get('id') === 'id_test_changed');
    +});
    +
    +test('If value of attribute "id" is set, value will be correctly reflect to original element.', (t) => {
    +  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
    +  const attributesContainer = new AttributesContainer(element);
    +  attributesContainer.set('id', 'id_test_changed');
    +  t.truthy(element.getAttribute('id') === 'id_test_changed');
    +});
    +
    +test('If value of attribute "class" is set, value will be correctly changed.', (t) => {
    +  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
    +  const attributesContainer = new AttributesContainer(element);
    +  t.truthy(attributesContainer.get('class') === 'class_test');
    +  attributesContainer.set('class', 'class_test_changed');
    +  t.truthy(attributesContainer.get('class') === 'class_test_changed');
    +});
    +
    +test('If value of attribute "class" is set, value will be correctly reflect to original element.', (t) => {
    +  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
    +  const attributesContainer = new AttributesContainer(element);
    +  attributesContainer.set('class', 'class_test_changed');
    +  t.truthy(element.getAttribute('class') === 'class_test_changed');
    +});
    +
    +test('If value of attribute "any_attribute" is set, value will be correctly changed.', (t) => {
    +  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
    +  const attributesContainer = new AttributesContainer(element);
    +  t.truthy(attributesContainer.get('any_attribute') === 'any_attribute_test');
    +  attributesContainer.set('any_attribute', 'any_attribute_test_changed');
    +  t.truthy(attributesContainer.get('any_attribute') === 'any_attribute_test_changed');
    +});
    +
    +test('If value of attribute "any_attribute" is set, value will not be reflect to original element.', (t) => {
    +  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
    +  const attributesContainer = new AttributesContainer(element);
    +  attributesContainer.set('any_attribute', 'any_attribute_test_changed');
    +  t.truthy(element.getAttribute('any_attribute') === 'any_attribute_test');
    +});

--- test/Goml/Attribute/AttributeContainerTest.js
+++ test/Goml/Attribute/AttributeContainerTest.js
@@ -32,3 +32,48 @@ test('Construct attribute container with correct attribute count (many)', (t) =>
   const attributesContainer = new AttributesContainer(element);
   t.truthy(Object.keys(attributesContainer._members).length === 3);
 });
+
+test('If value of attribute "id" is set, value will be correctly changed.', (t) => {
+  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
+  const attributesContainer = new AttributesContainer(element);
+  t.truthy(attributesContainer.get('id') === 'id_test');
+  attributesContainer.set('id', 'id_test_changed');
+  t.truthy(attributesContainer.get('id') === 'id_test_changed');
+});
+
+test('If value of attribute "id" is set, value will be correctly reflect to original element.', (t) => {
+  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
+  const attributesContainer = new AttributesContainer(element);
+  attributesContainer.set('id', 'id_test_changed');
+  t.truthy(element.getAttribute('id') === 'id_test_changed');
+});
+
+test('If value of attribute "class" is set, value will be correctly changed.', (t) => {
+  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
+  const attributesContainer = new AttributesContainer(element);
+  t.truthy(attributesContainer.get('class') === 'class_test');
+  attributesContainer.set('class', 'class_test_changed');
+  t.truthy(attributesContainer.get('class') === 'class_test_changed');
+});
+
+test('If value of attribute "class" is set, value will be correctly reflect to original element.', (t) => {
+  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
+  const attributesContainer = new AttributesContainer(element);
+  attributesContainer.set('class', 'class_test_changed');
+  t.truthy(element.getAttribute('class') === 'class_test_changed');
+});
+
+test('If value of attribute "any_attribute" is set, value will be correctly changed.', (t) => {
+  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
+  const attributesContainer = new AttributesContainer(element);
+  t.truthy(attributesContainer.get('any_attribute') === 'any_attribute_test');
+  attributesContainer.set('any_attribute', 'any_attribute_test_changed');
+  t.truthy(attributesContainer.get('any_attribute') === 'any_attribute_test_changed');
+});
+
+test('If value of attribute "any_attribute" is set, value will not be reflect to original element.', (t) => {
+  const element = new DOMParser().parseFromString('<tag id="id_test" class="class_test" any_attribute="any_attribute_test"></tag>', 'text/xml').documentElement;
+  const attributesContainer = new AttributesContainer(element);
+  attributesContainer.set('any_attribute', 'any_attribute_test_changed');
+  t.truthy(element.getAttribute('any_attribute') === 'any_attribute_test');
+});
