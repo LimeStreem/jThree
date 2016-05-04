6c1bbc857ba147cdb220819fa3d9d6ccc8e61bc4
pnlybubbles <pnlybubbles@gmail.com>
2016-05-04 11:15:43 +0900

    [from now] 2016/05/04 11:15:43
    diff --git a/src/Goml/Converter/FunctionConverter.ts b/src/Goml/Converter/FunctionConverter.ts
    index 61b8364..6969a27 100644
    --- a/src/Goml/Converter/FunctionConverter.ts
    +++ b/src/Goml/Converter/FunctionConverter.ts
    @@ -1,19 +1,18 @@
     import ConverterBase from "./ConverterBase";
    -import isFunction from "lodash.isfunction";
    
     class FunctionConverter extends ConverterBase {
       public name: string = "function";
    
       public toStringAttr(val: any): string {
    -    return val.toString().match(/function\sanonymous\(\)\s\{\n([\s\S]+)\n\}/m)[1];
    +    return val.toString().match(/function\sanonymous\(\)\s\{\n([\s\S]*)\n\}/m)[1];
       }
    
       public toObjectAttr(attr: any): () => any {
    -    return isFunction(attr) ? attr : new Function(attr);
    +    return this.validate(attr) ? attr : new Function(attr);
       }
    
       public validate(attr: any): boolean {
    -    return isFunction(attr);
    +    return (/function\sanonymous\(\)\s\{\n([\s\S]*)\n\}/m).test(attr.toString());
       }
     }

--- src/Goml/Converter/FunctionConverter.ts
+++ src/Goml/Converter/FunctionConverter.ts
@@ -1,19 +1,18 @@
 import ConverterBase from "./ConverterBase";
-import isFunction from "lodash.isfunction";
 
 class FunctionConverter extends ConverterBase {
   public name: string = "function";
 
   public toStringAttr(val: any): string {
-    return val.toString().match(/function\sanonymous\(\)\s\{\n([\s\S]+)\n\}/m)[1];
+    return val.toString().match(/function\sanonymous\(\)\s\{\n([\s\S]*)\n\}/m)[1];
   }
 
   public toObjectAttr(attr: any): () => any {
-    return isFunction(attr) ? attr : new Function(attr);
+    return this.validate(attr) ? attr : new Function(attr);
   }
 
   public validate(attr: any): boolean {
-    return isFunction(attr);
+    return (/function\sanonymous\(\)\s\{\n([\s\S]*)\n\}/m).test(attr.toString());
   }
 }
 
